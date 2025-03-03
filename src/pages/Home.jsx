2d23b46a716a40697afc06a85c3606297838dec1
FacundoVillarroel <facu.villarroel96@gmail.com>
2025-03-03 12:12:20 -0300

    [feature] - post task to db successfull

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,22 @@ const Home = () => {
   }, []);
 
   const onSubmit = async (formData) => {
-    console.log(formData);
+    try {
+      const response = await fetch("http://localhost:8080/api/tasks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (!response.ok) {
+        throw new Error("Error saving task");
+      }
+      const newTask = await response.json();
+      setTasks([...tasks, newTask]);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
